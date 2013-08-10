5bd58a679372d55c45f728e39a165f46125bfee9
wiedzmin137 <wiedzmin1371@gmail.com>
2013-08-10 15:06:48 +0200

    Test

--- PluginsPerDay/src/net/Servmine/MAHeroesAddon.java
+++ PluginsPerDay/src/net/Servmine/MAHeroesAddon.java
@@ -1,19 +1,14 @@
 package net.Servmine;
 
-import java.io.File;
 import java.util.logging.Logger;
 
 import com.garbagemule.MobArena.MobArena;
 import com.garbagemule.MobArena.framework.ArenaMaster;
-import com.herocraftonline.heroes.Heroes;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MAHeroesAddon extends JavaPlugin {
@@ -24,7 +19,6 @@ public final class MAHeroesAddon extends JavaPlugin {
 	    if (maPlugin == null)
 	        return;
 
-	    maListener = new MAaddon(maListener);
 	}
 	
     private static Logger log = Logger.getLogger("Minecraft");
@@ -36,27 +30,10 @@ public final class MAHeroesAddon extends JavaPlugin {
     {
         info = getDescription();
 
-        setupListeners();
         printToConsole("v" + info.getVersion() + " Wlaczono pomyslnie! Autorzy: " + info.getAuthors(), false);
     }
 	
-    private void setupListeners()
-    {
-        PluginManager pm = this.getServer().getPluginManager();
-        Heroes heroPlugin = (Heroes)pm.getPlugin("Heroes");
-        MobArena maPlugin = (MobArena)pm.getPlugin("MobArena");
-        
-        if(heroPlugin != null && heroPlugin.isEnabled()) {
-            pm.registerEvents(new MAaddon(), this);
-            printToConsole("Znaleziono Heroes!", false);
-        }
-        if(maPlugin != null && maPlugin.isEnabled()) {
-            am = maPlugin.getArenaMaster();
-            pm.registerEvents(new MAaddon(this), this);
-            foundMA = true;
-            printToConsole("Znaleziono Mob Arena!", false);
-        }
-    }
+
 
     public static void printToConsole(String msg, boolean warn)
     {
--- PluginsPerDay/src/net/Servmine/MAaddon.java
+++ PluginsPerDay/src/net/Servmine/MAaddon.java
@@ -3,37 +3,19 @@ package net.Servmine;
 import java.util.HashMap;
 import java.util.HashSet;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.garbagemule.MobArena.events.ArenaPlayerDeathEvent;
-import com.garbagemule.MobArena.events.ArenaPlayerJoinEvent;
 import com.garbagemule.MobArena.events.ArenaPlayerLeaveEvent;
 import com.garbagemule.MobArena.framework.Arena;
 import com.herocraftonline.heroes.api.events.SkillDamageEvent;
-import com.herocraftonline.heroes.characters.classes.HeroClass;
 
 public class MAaddon implements Listener
 {
     private HashMap<Arena,HashSet<String>> godMAMap = new HashMap<Arena,HashSet<String>>();
-    private MAaddon plugin;
-	private Player player;
-	
-    public MAaddon(MAaddon plugin)
-    {
-        this.plugin = plugin;
-    }
-  
-	@EventHandler
-    public void onMAPlayerJoin(ArenaPlayerJoinEvent event)
-    {
-        Player p = event.getPlayer();
-        HeroClass setExpieriece;
-        
-    }
 	
 	
     @EventHandler
