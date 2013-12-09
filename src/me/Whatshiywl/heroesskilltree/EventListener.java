3078b56c3e309b32dbfbbf3ca7006cd0fb60ee09
wiedzmin137 <wiedzmin1371@gmail.com>
2013-12-09 16:54:07 +0100

    Try to upadte

--- src/me/Whatshiywl/heroesskilltree/EventListener.java
+++ src/me/Whatshiywl/heroesskilltree/EventListener.java
@@ -25,7 +25,7 @@ import org.bukkit.inventory.ItemStack;
 public class EventListener implements Listener {
 
    private static HeroesSkillTree plugin;
-   
+
 
    public EventListener(HeroesSkillTree instance) {
       plugin = instance;
@@ -148,9 +148,6 @@ public class EventListener implements Listener {
          event.getHero().hasEffect(event.getSkill().getName());
          event.setCancelled(true);
       } else {
-         int health = (int)(SkillConfigManager.getUseSetting(hero, skill, "hst-health", 0.0D, false) * (double)(plugin.getSkillLevel(hero, skill) - 1));
-         health = health > 0?health:0;
-         event.setHealthCost(event.getHealthCost() + health);
          int mana = (int)(SkillConfigManager.getUseSetting(hero, skill, "hst-mana", 0.0D, false) * (double)(plugin.getSkillLevel(hero, skill) - 1));
          mana = mana > 0?mana:0;
          event.setManaCost(event.getManaCost() + mana);
