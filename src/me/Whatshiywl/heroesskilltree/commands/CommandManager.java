ef6ba6e92189f1edd93105b014a35936c8af658b
wiedzmin137 <wiedzmin1371@gmail.com>
2014-03-29 10:31:53 +0100

    Some upgrades to CommandManager
    It's propably not working.

--- src/me/Whatshiywl/heroesskilltree/commands/CommandManager.java
+++ src/me/Whatshiywl/heroesskilltree/commands/CommandManager.java
@@ -1,6 +1,7 @@
 package me.Whatshiywl.heroesskilltree.commands;
 
 import me.Whatshiywl.heroesskilltree.HeroesSkillTree;
+import me.Wiedzmin137.wheroesaddon.ItemGUI;
 import me.Wiedzmin137.wheroesaddon.Lang;
 
 import org.bukkit.command.Command;
@@ -19,31 +20,48 @@ public class CommandManager implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player) {
+			if (cmd.getName().equalsIgnoreCase("skilltree")) {
+				if (sender instanceof Player) {
+					if (args.length == 0) {
+						showInfoList(sender);
+						return true;
+					} else if (args.length > 0) {
+						switch (args[1]) {
+							case "admin": SkillAdminCommand.skillAdmin(plugin, sender, args); break;
+	        				case "down": SkillDownCommand.skillDown(plugin, sender, args); break;
+	        				case "info": SkillInfoCommand.skillInfo(plugin, sender, args); ; break;
+	        				case "list": SkillListCommand.skillList(plugin, sender, args); break;
+	        				case "unlocks": SkillLockedCommand.skillList(plugin, sender, args); break;
+	        				case "up": SkillUpCommand.skillUp(plugin, sender, args); break;
+	        				default: showInfoList(sender); break;
+						}
+					}
+					return true;
+				} else {
+					sender.sendMessage("There is some problem with colors in console, use /skilltree IN game, sorry.");
+				}
+				return true;
+			} else if (cmd.getName().equalsIgnoreCase("choose")) {
+				ItemGUI.createClassChoose((Player)sender);
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+	protected void showInfoList(CommandSender sender) {
 		Hero hero = HeroesSkillTree.heroes.getCharacterManager().getHero((Player)sender);
 		String skillPoints = String.valueOf(plugin.getCurrentPoints(hero, hero.getHeroClass()));
 		
-		if (cmd.getName().equalsIgnoreCase("skilltree")){
-        	if (sender instanceof Player) {
-        		if (args.length == 0) {
-        				sender.sendMessage(Lang.HELP_1.toString());
-        				sender.sendMessage(Lang.HELP_2.toString());
-        				sender.sendMessage(Lang.HELP_3.toString());
-        				sender.sendMessage(Lang.HELP_4.toString());
-        				sender.sendMessage(Lang.HELP_5.toString());
-        				sender.sendMessage(Lang.HELP_6.toString());
-        				sender.sendMessage(Lang.HELP_7.toString());
-        				sender.sendMessage(Lang.INFO_SKILLPOINTS.toString().replace("%points%", skillPoints));
-        		} else if (args.length > 0) {
-        			if (args[1] == "up") {
-        				SkillUpCommand.skillUp(plugin, sender, args);
-        			}
-				}
-				return true;
-			} else {
-				sender.sendMessage("There is some problem with colors in console, use /skilltree IN game, sorry.");
-			}
-			return true;
-		}
-		return false;
+		sender.sendMessage(Lang.HELP_1.toString());
+		sender.sendMessage(Lang.HELP_2.toString());
+		sender.sendMessage(Lang.HELP_3.toString());
+		sender.sendMessage(Lang.HELP_4.toString());
+		sender.sendMessage(Lang.HELP_5.toString());
+		sender.sendMessage(Lang.HELP_6.toString());
+		sender.sendMessage(Lang.HELP_7.toString());
+		sender.sendMessage(Lang.INFO_SKILLPOINTS.toString().replace("%points%", skillPoints));
 	}
 }
